a3ad1c3ed089d18c62f0d0a8647b70c36eafbbb5
Katie Pundt <kzpundt@gmail.com>
2022-02-26 16:51:30 -0800

    setup firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBkWpTeHjdefIW0ZhBVfPWMQTMFAF1lYmQ",
+  authDomain: "mymoney-14d1d.firebaseapp.com",
+  projectId: "mymoney-14d1d",
+  storageBucket: "mymoney-14d1d.appspot.com",
+  messagingSenderId: "921448533625",
+  appId: "1:921448533625:web:665d9e72959bb192da0ea8"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init service
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+export { projectFirestore, projectAuth };
