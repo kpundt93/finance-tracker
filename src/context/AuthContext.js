bfcbebd8ab068fa01dd4869f237688c5ce68ac15
Katie Pundt <kzpundt@gmail.com>
2022-02-26 17:48:50 -0800

    update AuthContext and dispatch signup action

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -2,8 +2,10 @@ import { createContext, useReducer } from 'react'
 
 export const AuthContext = createContext();
 
-export const AuthReducer = (state, action) => {
+export const authReducer = (state, action) => {
   switch (action.type) {
+    case 'LOGIN':
+      return { ...state , user: action.payload }
     default:
       return state;
   }
@@ -13,6 +15,7 @@ export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, {
     user: null
   });
+  console.log('AuthContext state:', state);
 
   return (
     <AuthContext.Provider value={{ ...state, dispatch }}>
