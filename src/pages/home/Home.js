12166d33bbcaea0f1c5dd83e0c42379f33395925
Katie Pundt <kzpundt@gmail.com>
2022-03-01 18:49:10 -0800

    implement useFirestore hook and reset form

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,17 +1,19 @@
 import React from 'react'
+import { useAuthContext } from '../../hooks/useAuthContext'
 // components
 import TransactionForm from './TransactionForm'
 // styles
 import styles from './Home.module.css'
 
 export default function Home() {
+  const { user } = useAuthContext();
   return (
     <div className={styles.container}>
       <div className={styles.content}>
         transaction list
       </div>
       <div className={styles.sidebar}>
-        <TransactionForm />
+        <TransactionForm uid={user.uid} />
       </div>
     </div>
   )
